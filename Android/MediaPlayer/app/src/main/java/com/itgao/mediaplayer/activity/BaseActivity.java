2950c2e8f16fe331960b2862efda8b4763d1b624
x-itgao <1148708206@qq.com>
2016-12-27 11:33:01 +0800

    media

---
+++ Android/MediaPlayer/app/src/main/java/com/itgao/mediaplayer/activity/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.itgao.mediaplayer.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by xiaogao.XU on 2016/12/26.
+ */
+public class BaseActivity extends Activity {
+    public static final String BROADCASTRECEIER_ACTIOn = "";
+
+}
